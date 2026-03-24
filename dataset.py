ba351a2268a9c6052b5ef36e6264dcf5354edc8c
abraar-raza <68822260+abraar-raza@users.noreply.github.com>
2026-03-24 18:23:37 -0400

    Add NWBDataset class for neural data handling
    Implement NWBDataset class for loading and processing unlabeled neural data from NWB files.

---
+++ dataset.py
@@ -0,0 +1,90 @@
+from torch.utils.data import Dataset
+from scipy.stats import zscore
+import os, glob, h5py
+
+class NWBDataset(Dataset):
+    def __init__(
+        self, root_dir, data_type='Both', duration=2,
+        channels_to_keep=None, very_bad_elecs=[234, 235], debug_mode=False
+    ):
+        """Dataset class for unlabeled data
+        Args:
+            data_type (str): HG | LFC | Both
+            duration (int): Length of a training sample in seconds
+        """
+        self.root_dir = root_dir
+        self.duration = duration
+        self.data_type = data_type
+        self.channels_to_keep = channels_to_keep if channels_to_keep\
+            is None else np.array(channels_to_keep)
+
+        self.very_bad_elecs = very_bad_elecs
+
+        self.samp_rate = 200
+        
+        nwb_files = glob.glob(
+            os.path.join(
+                self.root_dir, 'NZ*.nwb'
+            )
+        )
+
+        if debug_mode:
+            nwb_files = nwb_files[:3]
+
+        self.data = []
+        
+        for ii, nwb in enumerate(tqdm(nwb_files)):
+            with h5py.File(
+                nwb, mode='r', libver='latest',
+                swmr=True
+            ) as f:
+                if data_type.lower() == 'hg':
+                    raw_data = f['processing']['ecephys']['LFP']\
+                        ['high gamma (CAR 200.0Hz)']['data'][:]
+                elif data_type.lower() == 'lfc':
+                    raw_data = f['processing']['ecephys']['LFP']\
+                        ['preprocessed LFC (CAR 200.0Hz)']['data'][:]
+                elif data_type.lower() == 'both':
+                    raw_data = np.concat(
+                        (f['processing']['ecephys']['LFP']\
+                            ['high gamma (CAR 200.0Hz)']['data'][:],
+                        f['processing']['ecephys']['LFP']\
+                            ['preprocessed LFC (CAR 200.0Hz)']['data'][:]),
+                        axis=-1
+                    )
+
+                    if ii == 0:
+                        self.very_bad_elecs.extend(
+                            [x + 256 for x in self.very_bad_elecs]
+                        )
+                else:
+                    raise ValueError("data_type must be 'HG' or 'LFC' or 'Both'.")
+
+                raw_data = np.delete(raw_data, self.very_bad_elecs, axis=-1)
+                
+                self.data.append(raw_data)
+
+        num_of_duration_segments = [
+            len(dat)//self.samp_rate for dat in self.data
+        ]
+
+        self.all_duration_segments = [
+            [(nwb_idx, m_idx) for m_idx in range(min_idx)] for nwb_idx, min_idx
+            in zip(range(len(self.data)), num_of_duration_segments)
+        ]
+        self.all_duration_segments = np.concat(self.all_duration_segments).tolist()
+
+    def __len__(self):
+        return len(self.all_duration_segments)
+
+    def __getitem__(self, idx):
+        nwb_idx, segment_idx = self.all_duration_segments[idx]
+
+        norm_data = zscore(
+            self.data[nwb_idx][segment_idx*self.duration*200 : (segment_idx+1)*self.duration*200],
+            axis=0
+        )
+        
+        tensor_data = torch.from_numpy(norm_data)
+
+        return tensor_data
