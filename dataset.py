1fc63bbc9fa3d3038ab329ea2b9c83bee04b93fb
abraar-raza <68822260+abraar-raza@users.noreply.github.com>
2026-03-24 19:04:30 -0400

    Refactor NWBDataset to handle EKG channels and sampling rate
    Updated NWBDataset class to handle EKG channels and sampling rate correctly.

--- dataset.py
+++ dataset.py
@@ -4,56 +4,78 @@ import os, glob, h5py, torch
 from tqdm import tqdm 
 import numpy as np
 
-
 class NWBDataset(Dataset):
     def __init__(
         self, root_dir, data_type='Both', duration=2,
-        channels_to_keep=None, very_bad_elecs=[234, 235], debug_mode=False
+        channels_to_keep=None, debug_mode=False
     ):
         """Dataset class for unlabeled data
         Args:
             data_type (str): HG | LFC | Both
             duration (int): Length of a training sample in seconds
+            channels_to_keep: Tell which channels to keep. Must be 0-indexed. Note: EKG channels...
+            ... have already been removed internally in this class.
         """
         self.root_dir = root_dir
         self.duration = duration
         self.data_type = data_type
         self.channels_to_keep = channels_to_keep if channels_to_keep\
             is None else np.array(channels_to_keep)
-        self.very_bad_elecs = very_bad_elecs
-        self.samp_rate = 200
+
+        self.samp_rate = None
+        
         nwb_files = glob.glob(
             os.path.join(
                 self.root_dir, 'NZ*.nwb'
             )
         )
+
         if debug_mode:
             nwb_files = nwb_files[:3]
+
         self.data = []
+        
         for ii, nwb in enumerate(tqdm(nwb_files)):
             with h5py.File(
                 nwb, mode='r', libver='latest',
                 swmr=True
             ) as f:
+                if ii == 0:
+                    ekg_channels = raw_data['general']['extracellular_ephys']['electrodes']['bad'][:]
+                
                 if data_type.lower() == 'hg':
                     raw_data = f['processing']['ecephys']['LFP']\
-                        ['high gamma (CAR 200.0Hz)']['data'][:]
+                        ['high gamma (CAR 200.0Hz)']['data'][:, ~ekg_channels]
+
+                    if self.samp_rate is None:
+                        self.samp_rate = f['processing']['ecephys']['LFP']\
+                        ['high gamma (CAR 200.0Hz)']['starting_time'].attrs['rate'].item()
+
                 elif data_type.lower() == 'lfc':
                     raw_data = f['processing']['ecephys']['LFP']\
-                        ['preprocessed LFC (CAR 200.0Hz)']['data'][:]
+                        ['preprocessed LFC (CAR 200.0Hz)']['data'][:, ~ekg_channels]
+
+                    if self.samp_rate is None:
+                        self.samp_rate = f['processing']['ecephys']['LFP']\
+                        ['preprocessed LFC (CAR 200.0Hz)']['starting_time'].attrs['rate'].item()
                 elif data_type.lower() == 'both':
                     raw_data = np.concat(
                         (f['processing']['ecephys']['LFP']\
-                            ['high gamma (CAR 200.0Hz)']['data'][:],
+                            ['high gamma (CAR 200.0Hz)']['data'][:, ~ekg_channels],
                         f['processing']['ecephys']['LFP']\
-                            ['preprocessed LFC (CAR 200.0Hz)']['data'][:]),
+                            ['preprocessed LFC (CAR 200.0Hz)']['data'][:, ~ekg_channels]),
                         axis=-1
                     )
 
-                    if ii == 0:
-                        self.very_bad_elecs.extend(
-                            [x + 256 for x in self.very_bad_elecs]
-                        )
+                    if self.samp_rate is None:
+                        hg_rate = f['processing']['ecephys']['LFP']\
+                        ['high gamma (CAR 200.0Hz)']['starting_time'].attrs['rate'].item()
+                        lfc_rate = f['processing']['ecephys']['LFP']\
+                        ['preprocessed LFC (CAR 200.0Hz)']['starting_time'].attrs['rate'].item()
+
+                        assert hg_rate == lfc_rate, "High Gamma and LFCs must have the same sampling rate."
+
+                        self.samp_rate = hg_rate
                 else:
                     raise ValueError("data_type must be 'HG' or 'LFC' or 'Both'.")
 
@@ -77,11 +99,17 @@ class NWBDataset(Dataset):
     def __getitem__(self, idx):
         nwb_idx, segment_idx = self.all_duration_segments[idx]
 
+        start_idx = segment_idx * self.duration * self.samp_rate
+        end_idx = start_idx + self.duration * self.samp_rate
+        selected_data = self.data[nwb_idx][start_idx:end_idx]
+
         norm_data = zscore(
-            self.data[nwb_idx][segment_idx*self.duration*self.samp_rate : (segment_idx+1)*self.duration*self.samp_rate],
+            selected_data,
             axis=0
         )
+        
         tensor_data = torch.from_numpy(norm_data)
+
         return tensor_data
 
 
